3014b5992e80b5615ff6e5ed78510ad5db7f348f
Lorenzo1799 <lorenzogwada@gmail.com>
2024-05-07 23:06:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import time
+
+start_time = time.time()
+
+pages = []
+for page_number in range(1, 5):
+    url_start = 'https://www.centralcharts.com/it/quotazioni-rialzi-ribassi/'
+    url_end = 'ALL/desc/ts_19-us-nasdaq-azioni--qc_2-variazione-prezzo-giornaliero?p='
+    url = url_start + url_end + str(page_number)
+    pages.append(url)
+
+values_list = []
+for page in pages:
+    webpage = requests.get(page)
+    soup = bs(webpage.text, 'html.parser')
+
+    stock_table = soup.find('table', class_= 'tabMini tabQuotes')
+    tr_tag_list = stock_table.find_all('tr')
+
+    for each_tr_tag in tr_tag_list[1:]:
+        td_tag_list = each_tr_tag.find_all('td')
+
+        row_values = []
+        for each_td_tag in td_tag_list[0:7]:
+            new_value = each_td_tag.text.strip()
+            row_values.append(new_value)
+
+        values_list.append(row_values)
+
+print(values_list)
+print('- - - %s seconds - - -' % (time.time() - start_time))
+
